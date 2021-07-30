aace4fc6bc91b3a4c390ccb39470e3438dca1c1d
Tony Aguilera <toni.aguilera@cabsa.es>
2021-07-30 10:26:37 +0200

    First commit

---
+++ src/main/java/toni/aguilera/spaceStation/SpaceStationApplication.java
@@ -0,0 +1,13 @@
+package toni.aguilera.spaceStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toni/aguilera/spaceStation/SpaceStationApplicationTests.java
@@ -0,0 +1,13 @@
+package toni.aguilera.spaceStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
